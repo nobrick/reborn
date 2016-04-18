926927c616f28972ddcb78a922fde7142f8d7ac6
Ming Qu <qucool@gmail.com>
2016-04-18 23:09:32 +0800

    Support multiple modes for Huo.Market.get/1

--- apps/huo/lib/huo/market.ex
+++ apps/huo/lib/huo/market.ex
@@ -1,10 +1,15 @@
 defmodule Huo.Market do
-  @simple_base  'http://api.huobi.com/staticmarket/ticker_btc_json.js'
-  @detail_base  'http://api.huobi.com/staticmarket/detail_btc_json.js'
+  @simple_base "http://api.huobi.com/staticmarket/ticker_btc_json.js"
+  @detail_base "http://api.huobi.com/staticmarket/detail_btc_json.js"
+  @kline_1min_base "http://api.huobi.com/staticmarket/btc_kline_001_json.js_json.js"
 
-  def get(:simple) do
+  def get(:simple), do: get(:simple, @simple_base)
+  def get(:detail), do: get(:detail, @detail_base)
+  def get(:kline), do: get(:kline, @kline_1min_base)
+
+  defp get(_mode, base_url) do
     HTTPoison.start
-    response = HTTPoison.get(@simple_base)
+    response = HTTPoison.get(base_url)
     case response do
       {:ok, %{status_code: 200, body: body} = result} ->
         body = body |> Poison.decode!
--- apps/huo/test/huo/market_test.exs
+++ apps/huo/test/huo/market_test.exs
@@ -2,6 +2,14 @@ defmodule Huo.MarketTest do
   use ExUnit.Case
 
   test "get :simple" do
-    Huo.Market.get(:simple) |> inspect |> IO.puts
+    assert {:ok, _} = Huo.Market.get(:simple)
+  end
+
+  test "get :detail" do
+    assert {:ok, _} = Huo.Market.get(:detail)
+  end
+
+  test "get :kline" do
+    assert {:ok, _} = Huo.Market.get(:kline)
   end
 end
