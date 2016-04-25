e0674e789495ecf30af16dc114826af6142028cb
MQ <qucool@gmail.com>
2016-04-25 17:24:12 +0800

    Add interval tests

--- apps/caravan/test/caravan/wheel/interval_test.exs
+++ apps/caravan/test/caravan/wheel/interval_test.exs
@@ -8,15 +8,20 @@ defmodule Caravan.Wheel.IntervalTest do
   setup do
     :ok = Sandbox.checkout(Repo)
     {:ok, wheel} = Wheel.start_link
+    Sandbox.allow(Repo, self(), wheel)
     {:ok, interval_sup} = Task.Supervisor.start_link
-    {:ok, wheel: wheel, interval_sup: interval_sup}
+    {:ok, wheel: wheel, interval_sup: interval_sup, pulling_interval: 1000, test_process: self()}
   end
 
-  # TODO: Get able to test the interval module.
   test "start_link/2 pulls at a regular interval", context do
     opts = context
-    |> Map.take([:wheel, :interval_sup])
+    |> Map.take([:wheel, :interval_sup, :pulling_interval, :test_process])
     |> Map.to_list
-    {:ok, interval} = Interval.start_link(opts)
+    {:ok, _interval} = Interval.start_link(opts)
+    for _ <- 1..3 do
+      assert_receive {:start_child, pid}, 3000
+      ref = Process.monitor(pid)
+      assert_receive {:DOWN, ^ref, :process, _, :normal}, 3000
+    end
   end
 end
