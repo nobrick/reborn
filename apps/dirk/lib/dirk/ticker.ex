9d697bec44f406d7d39973126d9a4fe8268ab8af
MQ <qucool@gmail.com>
2016-04-28 09:55:23 +0800

    Support time range offset for in_time_range/4

--- apps/dirk/lib/dirk/ticker.ex
+++ apps/dirk/lib/dirk/ticker.ex
@@ -2,6 +2,7 @@ defmodule Dirk.Ticker do
   use Ecto.Schema
   import Ecto.Changeset
   import Ecto.Query, only: [from: 2]
+  alias Dirk.Ticker
 
   schema "ticker" do
     field :op, :float
@@ -24,10 +25,19 @@ defmodule Dirk.Ticker do
     |> cast(params, @required_fields, @optional_fields)
   end
 
-  def in_time_range(query, time, margin) do
+  @doc """
+  Queries in the given time range.
+
+    * `query`: The input query for chaining.
+    * `time`: The base time.
+    * `margin`: The margin of the time range in the unit of `second`.
+    * `offset`: The range offset based on `time` in the unit of `second`.
+                Defaults to 0.
+  """
+  def in_time_range(query, time, margin, offset \\ 0) do
     time = Ecto.DateTime.cast!(time)
     from(t in query,
-      where: t.time > datetime_add(^time, ^-margin, "second")
-         and t.time < datetime_add(^time, ^margin, "second"))
+      where: t.time > datetime_add(^time, ^(offset - margin), "second")
+         and t.time < datetime_add(^time, ^(offset + margin), "second"))
   end
 end
--- apps/dirk/test/dirk/ticker_test.exs
+++ apps/dirk/test/dirk/ticker_test.exs
@@ -1,15 +1,15 @@
 defmodule Dirk.TickerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   import Ecto.DateTime, only: [cast!: 1]
-  import Dirk.Ticker, only: [in_time_range: 3]
+  import Dirk.Ticker, only: [in_time_range: 3, in_time_range: 4]
   alias Ecto.Adapters.SQL.Sandbox
   alias Dirk.Ticker
   alias Dirk.Repo
 
   setup do
     :ok = Sandbox.checkout(Repo)
-    for sec <- [30, 31, 32] do
-      time = {{2016, 4, 20}, {13, 00, sec}}
+    for time_part <- [{13, 0, 58}, {13, 0, 59}, {13, 1, 0}] do
+      time = {{2016, 4, 20}, time_part}
       ticker = %Ticker{op: 2900.0, la: 3000.0, hi: 3100.0, lo: 2800.0,
                        vo: 1600000.0, of: 3000.0, bi: 3000.0, d_la: 0.00001,
                        time: cast!(time)}
@@ -18,17 +18,25 @@ defmodule Dirk.TickerTest do
     :ok
   end
 
-  test "in_time_range/3 returns the expected query" do
+  test "in_time_range/4 returns the expected query" do
     assert [%Ticker{}] = Ticker
-    |> in_time_range({{2016, 4, 20}, {13, 00, 31}}, 1)
-    |> Repo.all
+    |> in_time_range({{2016, 4, 20}, {13, 0, 59}}, 1) |> Repo.all
 
     assert [%Ticker{}, %Ticker{}, %Ticker{}] = Ticker
-    |> in_time_range({{2016, 4, 20}, {13, 00, 31}}, 1.01)
-    |> Repo.all
+    |> in_time_range({{2016, 4, 20}, {13, 0, 59}}, 1.01) |> Repo.all
 
     assert [%Ticker{}, %Ticker{}] = Ticker
-    |> in_time_range({{2016, 4, 20}, {13, 00, 30}}, 1.01)
-    |> Repo.all
+    |> in_time_range({{2016, 4, 20}, {13, 1, 0}}, 1.01) |> Repo.all
+  end
+
+  test "in_time_range/4 handles time range offset" do
+    assert [%Ticker{}, %Ticker{}, %Ticker{}] = Ticker
+    |> in_time_range({{2016, 4, 20}, {13, 0, 49}}, 1.01, 10) |> Repo.all
+
+    assert [%Ticker{}] = Ticker
+    |> in_time_range({{2016, 4, 20}, {12, 59, 49}}, 0.5, 70) |> Repo.all
+
+    assert [%Ticker{}, %Ticker{}, %Ticker{}] = Ticker
+    |> in_time_range({{2016, 4, 20}, {13, 1, 59}}, 1.01, -60) |> Repo.all
   end
 end
