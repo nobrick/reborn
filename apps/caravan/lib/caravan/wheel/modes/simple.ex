6da1080adb8a9222eb5ce9628db0f0635108619e
MQ <qucool@gmail.com>
2016-06-13 09:29:58 +0800

    Mark Wheel.Modes helper methods as private

--- apps/caravan/lib/caravan/wheel/modes/simple.ex
+++ apps/caravan/lib/caravan/wheel/modes/simple.ex
@@ -17,9 +17,9 @@ defmodule Caravan.Wheel.Modes.Simple do
 
   ## Helpers
 
-  def map_to_ticker(body), do: struct!(Ticker, map_resp(body))
+  defp map_to_ticker(body), do: struct!(Ticker, map_resp(body))
 
-  def map_resp(%{"ticker" => %{"buy" => bi, "high" => hi, "last" => la, "low"
+  defp map_resp(%{"ticker" => %{"buy" => bi, "high" => hi, "last" => la, "low"
       => lo, "open" => op, "sell" => of, "vol" => vo}, "time" => time}) do
     time = time
     |> String.to_integer
@@ -27,7 +27,7 @@ defmodule Caravan.Wheel.Modes.Simple do
     %{op: op, la: la, hi: hi, lo: lo, vo: vo, of: of, bi: bi, time: time}
   end
 
-  def insert(body) do
+  defp insert(body) do
     Ticker.changeset(:create, %Ticker{}, map_resp(body))
     |> Repo.insert
   end
