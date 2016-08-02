ff6726bc0a15f5909b1938e9ef19838ef807f589
Ming Qu <qucool@gmail.com>
2016-08-02 20:34:09 +0800

    Take care of float values in Huo.Order responses
    Add Utils.Param.format_resp/1

--- apps/utils/test/utils/param_test.exs
+++ apps/utils/test/utils/param_test.exs
@@ -1,4 +1,17 @@
 defmodule Utils.ParamTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  alias Utils.Param
   doctest Utils.Param
+
+  setup do
+    resp = %{"fee" => "0.00", "id" => 16, "total" => "0.81"}
+    expected_resp = %{"fee" => 0, "id" => 16, "total" => 0.81}
+    {:ok, resp: resp, expected_resp: expected_resp}
+  end
+
+  describe "format_resp/1" do
+    test "returns expected value", %{resp: resp, expected_resp: expected} do
+      assert Param.format_resp(resp) == expected
+    end
+  end
 end
