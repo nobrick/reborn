9673a87680df8bc4569013785ce089987c6c9320
Ming Qu <qucool@gmail.com>
2016-08-13 22:52:15 +0800

    Update dependencies
    Add GenStage.

--- apps/caravan/mix.exs
+++ apps/caravan/mix.exs
@@ -8,7 +8,7 @@ defmodule Caravan.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -18,7 +18,7 @@ defmodule Caravan.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :dirk, :huo, :utils],
+    [applications: [:logger, :dirk, :huo, :utils, :gen_stage],
      mod: {Caravan, []}]
   end
 
@@ -36,6 +36,9 @@ defmodule Caravan.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:dirk, in_umbrella: true}, {:huo, in_umbrella: true}, {:utils, in_umbrella: true}]
+    [{:dirk, in_umbrella: true},
+     {:huo, in_umbrella: true},
+     {:utils, in_umbrella: true},
+     {:gen_stage, "~> 0.4"}]
   end
 end
