deac8eeed78f330275c9a2b5ce0262f8a710b618
Ming Qu <qucool@gmail.com>
2016-08-24 19:04:53 +0800

    Support WatcherSupervisor

--- apps/azor/lib/azor/ords/watcher_supervisor.ex
+++ apps/azor/lib/azor/ords/watcher_supervisor.ex
@@ -7,6 +7,15 @@ defmodule Azor.Ords.WatcherSupervisor do
     Supervisor.start_link(__MODULE__, :ok, opts)
   end
 
+  def start_child(args, opts \\ [])
+      when is_map(args) and is_list(opts) do
+    Supervisor.start_child(__MODULE__, [args, opts])
+  end
+
+  def terminate_child(pid) do
+    Supervisor.terminate_child(__MODULE__, pid)
+  end
+
   def init(:ok) do
     children = [
       worker(@watcher, [], restart: :transient)
