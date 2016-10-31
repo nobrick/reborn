f953d62a78b014248afb5020a3a1082abd7b24fd
Ming Qu <qucool@gmail.com>
2016-10-31 18:27:13 +0800

    Use independent interval defaults

--- apps/caravan/lib/caravan/wheel/interval.ex
+++ apps/caravan/lib/caravan/wheel/interval.ex
@@ -3,19 +3,25 @@ defmodule Caravan.Wheel.Interval do
   import Task.Supervisor, only: [start_child: 4]
   alias Caravan.Wheel
 
+  @interval_sup Wheel.Interval.Supervisor
+  @pulling_interval_defaults %{simple: 5_000, k15: 60_000}
+
   @doc """
   Starts Caravan.Wheel.Interval instance.
 
   ## Options
 
   - `interval_sup`: The `Task.Supervisor` for supervising periodically
-  pulling processes. `Caravan.Wheel.Interval.Supervisor` by default.
+    pulling processes. `Caravan.Wheel.Interval.Supervisor` by default.
 
   - `wheel`: The wheel instance for pulling. `Caravan.Wheel` by default.
 
-  - `mode`: The mode for pulling. :simple by default.
+  - `mode`: The mode for pulling. available options: `:simple`, `k15`.
+    `:simple` by default.
 
-  - `pulling_interval`: The interval to trigger pulling. 5000 by default.
+  - `pulling_interval`: The interval to trigger pulling. Default values:
+    * #{@pulling_interval_defaults.simple} for simple mode,
+    * #{@pulling_interval_defaults.k15} for k15 mode.
 
   - `test_process`: The message monitoring process for testing purposes.
   """
@@ -23,14 +29,12 @@ defmodule Caravan.Wheel.Interval do
     GenServer.start_link(__MODULE__, extract_options(opts))
   end
 
-  @interval_sup Wheel.Interval.Supervisor
-  @pulling_interval 5000
-
   defp extract_options(opts) do
     {wheel, opts} = Keyword.pop(opts, :wheel, Wheel)
     {mode, opts} = Keyword.pop(opts, :mode, :simple)
     {interval_sup, opts} = Keyword.pop(opts, :interval_sup, @interval_sup)
-    {interval, opts} = Keyword.pop(opts, :pulling_interval, @pulling_interval)
+    {interval, opts} = Keyword.pop(opts, :pulling_interval,
+                                   @pulling_interval_defaults[mode])
     {test_process, _} = Keyword.pop(opts, :test_process, nil)
     %{wheel: wheel,
       mode: mode,
