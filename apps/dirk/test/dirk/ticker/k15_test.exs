3502c38edbc0edda1c1e85cee51b4d6930aa0055
Ming Qu <qucool@gmail.com>
2016-11-01 19:03:35 +0800

    Add Dirk.Ticker.K15.distinct_on_time/1

--- apps/dirk/test/dirk/ticker/k15_test.exs
+++ apps/dirk/test/dirk/ticker/k15_test.exs
@@ -1,3 +1,16 @@
 defmodule Dirk.Ticker.K15Test do
   use ExUnit.Case, async: true
+  import Dirk.Ticker.K15, only: [distinct_on_time: 1]
+  alias Dirk.Ticker.K15
+  alias Dirk.Repo
+
+  test "distinct_on_time/1" do
+    import Ecto.DateTime, only: [cast!: 1]
+    query = distinct_on_time(K15)
+    sql_head = ~S{SELECT DISTINCT ON (k0."time")}
+    sql_tail = ~S{ORDER BY k0."time" DESC, k0."vo" DESC}
+    {sql, []} = Ecto.Adapters.SQL.to_sql(:all, Repo, query)
+    assert String.starts_with?(sql, sql_head)
+    assert String.ends_with?(sql, sql_tail)
+  end
 end
