b09ba147a0cfef66ff60f91fd7707efd122872af
Ming Qu <qucool@gmail.com>
2016-11-06 23:20:00 +0800

    Initialize Machine app

---
+++ apps/machine/README.md
@@ -0,0 +1,24 @@
+# Machine
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `machine` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:machine, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `machine` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:machine]]
+    end
+    ```
+
---
+++ apps/machine/lib/machine.ex
@@ -0,0 +1,20 @@
+defmodule Machine do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Machine.Worker.start_link(arg1, arg2, arg3)
+      # worker(Machine.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Machine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
