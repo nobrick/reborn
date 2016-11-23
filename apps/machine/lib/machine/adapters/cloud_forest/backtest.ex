736cefd3b3e434b95c94015add9487afa0dc4863
Ming Qu <qucool@gmail.com>
2016-11-23 23:41:49 +0800

    Use standalone data directories for backtesting
    Add `profit/2` function for `CloudForest.Backtest`.
    Update `gen_stage` dependency.

--- apps/machine/lib/machine/adapters/cloud_forest/backtest.ex
+++ apps/machine/lib/machine/adapters/cloud_forest/backtest.ex
@@ -3,39 +3,28 @@ defmodule Machine.Adapters.CloudForest.Backtest do
   A module for backtesting the data.
   """
 
-  use GenServer
+  alias Experimental.Flow
   alias Machine.Corr
   alias Machine.Adapters.CloudForest.{Predictor, Trainer}
 
-  ## API
-
-  def start_link(args \\ %{}, opts \\ []) do
-    GenServer.start_link(__MODULE__, args, opts)
-  end
-
-  ## Callbacks
-
-  def init(args) do
-    {:ok, args}
-  end
-
-  ## Helpers
-
   @filters Application.get_env(:machine, :corr_filters)
+  @data_storage_path Application.get_env(:machine, :data_storage_path)
+  @ev_threshold 0.0011
+  @profit_threshold 0.0011
 
   @doc """
   Backtests for one.
   """
-  def test_one(target_chunk, chunks, filters \\ @filters) do
+  def test_one(dir_path, target_chunk, chunks, filters \\ @filters) do
     pattern = target_chunk |> Enum.map(& &1.d_la) |> tl
     corr_chunks = Corr.find_corr_chunks(chunks, pattern, :d_la)
     case Corr.filter_corr_chunks(corr_chunks, filters) do
-      {:ok, log, filtered} ->
-        Trainer.save_data(filtered |> Stream.map(& elem(&1, 0)))
-        Trainer.learn
-        Predictor.save_data(target_chunk)
-        Predictor.predict
-        {:ok, log, Predictor.read_prediction}
+      {:ok, logs, filtered} ->
+        Trainer.save_data(dir_path, filtered |> Stream.map(& elem(&1, 0)))
+        Trainer.learn(dir_path)
+        Predictor.save_data(dir_path, target_chunk)
+        Predictor.predict(dir_path)
+        {:ok, logs, Predictor.read_prediction(dir_path)}
       {:error, _} = error ->
         error
     end
@@ -45,6 +34,18 @@ defmodule Machine.Adapters.CloudForest.Backtest do
   Backtests for all.
   """
   def test_all(target_chunks, lookup_chunks, opts \\ []) do
+    if Keyword.get(opts, :check_target_lookup_conflicts, true) do
+      range = fn chunks ->
+        [List.last(List.last(chunks)).time,
+         hd(hd(chunks)).time,
+         Enum.count(chunks)]
+      end
+      # TODO: Checks if the two periods conflict each other instead of
+      # outputing results.
+      range.(target_chunks) |> IO.inspect
+      range.(lookup_chunks) |> IO.inspect
+    end
+
     fun = fn -> do_test_all(target_chunks, lookup_chunks) end
     if Keyword.get(opts, :tc, true) do
       {time, value} = :timer.tc(fun)
@@ -54,28 +55,59 @@ defmodule Machine.Adapters.CloudForest.Backtest do
     end
   end
 
-  def do_test_all(target_chunks, lookup_chunks) do
+  defp do_test_all(target_chunks, lookup_chunks) do
     chunks_count = Enum.count(target_chunks)
+    data_dir_path = make_data_dir
     result =
       target_chunks
       |> Stream.with_index
       |> Enum.map(fn {target, index} ->
         IO.puts "---- #{index}..#{chunks_count - 1} ----"
-        test_one(target, lookup_chunks) |> IO.inspect
+        subdir_path = Path.join(data_dir_path, Integer.to_string(index))
+        File.mkdir!(subdir_path)
+        test_one(subdir_path, target, lookup_chunks) |> IO.inspect
       end)
-    expections =
-      result
-      |> Enum.filter_map(fn {:ok, _, _} -> true; {:error, _} -> false end,
-                         fn {:ok, _, [_, p, a]} -> calc_ev(p, a) end)
-    count_ev = fn n -> expections |> Stream.filter(& &1 == n) |> Enum.count end
-    pos_count = count_ev.(1)
-    neg_count = count_ev.(-1)
-    zero_count = count_ev.(0)
-    %{result: result, expections: expections,
-      stats: %{chunks_count: chunks_count, pos_count: pos_count,
-               neg_count: neg_count, zero_count: zero_count,
-               rate: safe_div(pos_count, (pos_count + neg_count))},
+    p_a_list = Enum.filter_map(result,
+                 fn {:ok, _, _} -> true; {:error, _} -> false end,
+                 fn {:ok, _, [_, p, a]} -> {p, a} end)
+    %{result: result, p_a_list: p_a_list, profit: profit(p_a_list),
+      stats: %{chunks_count: chunks_count},
       corr_filters: get_corr_filters_stats(result)}
+    |> Map.update!(:stats, & Map.merge(&1, count_ev(p_a_list)))
+  end
+
+  defp make_data_dir do
+    {{y, m, d}, {hh, mm, ss}} = :calendar.universal_time
+    path = Path.join(@data_storage_path, "#{y}-#{m}-#{d}-#{hh}-#{mm}-#{ss}")
+    File.mkdir!(path)
+    path
+  end
+
+  @doc """
+  Counts expection values.
+  """
+  def count_ev(p_a_list, calc_ev_fun \\ &calc_ev/2) do
+    expections = Enum.map(p_a_list, fn {p, a} -> calc_ev_fun.(p, a) end)
+    count = fn n -> expections |> Stream.filter(& &1 == n) |> Enum.count end
+    pos_count = count.(1)
+    neg_count = count.(-1)
+    zero_count = count.(0)
+    %{pos_count: pos_count,
+      neg_count: neg_count,
+      zero_count: zero_count,
+      rate: safe_div(pos_count, (pos_count + neg_count))}
+  end
+
+  @doc """
+  Calculates the profit expectation given the prediction value filter `p_fun`.
+  """
+  def profit(p_a_list, p_fun \\ &(&1 >= @profit_threshold)) do
+    p_a_list
+    |> Enum.filter(fn {p, _} -> p_fun.(p) end)
+    |> Enum.reduce({1, 0}, fn {_, a}, {value, count} ->
+      {value * (1 + a), count + 1}
+    end)
+    |> (fn {value, count} -> {value - 1, count} end).()
   end
 
   defp get_corr_filters_stats(result) do
@@ -95,14 +127,18 @@ defmodule Machine.Adapters.CloudForest.Backtest do
     end
   end
 
-  defp calc_ev(predicted, actual, threshold \\ 0.0011) do
+  defp calc_ev(predicted, actual, threshold \\ @ev_threshold) do
     cond do
-      abs(predicted) < threshold ->
-        0
-      actual >= 0 ->
+      predicted >= threshold && actual >= 0 ->
         1
-      true ->
+      predicted >= threshold ->
         -1
+      predicted < -threshold && actual < 0 ->
+        1
+      predicted < -threshold ->
+        -1
+      true ->
+        0
     end
   end
 end
--- apps/machine/lib/machine/adapters/cloud_forest/trainer.ex
+++ apps/machine/lib/machine/adapters/cloud_forest/trainer.ex
@@ -5,26 +5,25 @@ defmodule Machine.Adapters.CloudForest.Trainer do
 
   alias Machine.Adapters.CloudForest.DataWriter
 
-  @samples_path "samples.trans.fm"
-  @forest_path "forest.sf"
+  @samples_name "samples.trans.fm"
+  @forest_name "forest.sf"
 
   @doc """
   Trains the data.
   """
-  def learn(opts \\ []) do
-    samples_path = opts[:samples_path] || @samples_path
-    forest_path = opts[:forest_path] || @forest_path
-    args = "-train=#{samples_path} -rfpred=#{forest_path} -target=N:d_la0 " <>
-           "-nTrees=100 -mTry=.33 -oob=false -nCores=1" |> String.split
-    {result, 0} = System.cmd("growforest", args)
+  def learn(dir_path) do
+    args =
+      "-train=#{@samples_name} -rfpred=#{@forest_name} -target=N:d_la0 " <>
+      "-nTrees=100 -mTry=.33 -oob=false -nCores=1" |> String.split
+    {result, 0} = System.cmd("growforest", args, cd: dir_path)
     IO.puts result
   end
 
   @doc """
   Saves the data for training.
   """
-  def save_data(chunks, opts \\ []) do
-    samples_path = opts[:samples_path] || @samples_path
+  def save_data(dir_path, chunks) do
+    samples_path = Path.join(dir_path, @samples_name)
     DataWriter.save_data(DataWriter, samples_path, chunks)
   end
 end
