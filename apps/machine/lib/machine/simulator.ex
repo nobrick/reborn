33d34391f55f52b07b7f80231ae6255afcbabc51
Ming Qu <qucool@gmail.com>
2017-01-10 00:02:13 +0800

    Update Machine.Simulator

--- apps/machine/lib/machine/simulator.ex
+++ apps/machine/lib/machine/simulator.ex
@@ -11,7 +11,7 @@ defmodule Machine.Simulator do
   @compile {:inline, init_seq_pft_state: 0, ma_s: 1, ma_m: 1, ma_l: 1, la: 1}
   @pft_threshold Application.get_env(:machine, :pft_threshold)
   @seq_names ~w(gt_threshold gt_0 exp_1 exp_2 exp_3 e4 la_gt_ma_s ma_e4 ma_e5
-                ma_e45 ma_e45s ma_gt_0 ma_t0 ma_pure_0)a
+                ma_e45 ma_e45s ma_gt_0 s_t0 ma_pure_0)a
 
   @doc """
   Simulates each sequence strategies and generate the pfts and instructions.
@@ -19,7 +19,7 @@ defmodule Machine.Simulator do
   def test_sequence_pfts(result, seq_names \\ @seq_names) do
     {seq_info, seq_lists} =
       seq_names
-      |> Enum.map(fn n -> sequence_pft(result, & seq_r_fun(n, &1)) end)
+      |> Enum.map(fn n -> sequence_pft(result, & seq_r_fun(n, &1, &2)) end)
       |> Enum.unzip
     seq_info = seq_names |> Enum.map(& :"seq_#{&1}") |> Enum.zip(seq_info)
     seq_lists = Enum.zip(seq_names, seq_lists)
@@ -40,9 +40,9 @@ defmodule Machine.Simulator do
   @doc """
   Writes all seq_lists into a single CSV file.
   
-  The first four columns are: :index, :decision, :holds, :ba, :d_la_initial.
-  Among them, :decision, :holds and :ba are corresponding to the first method
-  in the methods list
+  The first four columns are: :index, :instruction, :holds, :ba, :d_la_initial.
+  Among them, :instruction, :holds and :ba are corresponding to the first
+  method in the methods list
   """
   def put_summary_csv(seq_lists, opts \\ []) do
     path = opts[:path] || "summary.txt"
@@ -64,8 +64,8 @@ defmodule Machine.Simulator do
   defp put_seq_to_stream(seq_list, method) when is_atom(method) do
     seq_list
     |> Stream.with_index(1)
-    |> Stream.map(fn {{decision, holds, ba, la, pft}, index} ->
-      [index, inspect(decision), holds, ba, la, pft]
+    |> Stream.map(fn {{instruction, holds, ba, la, pft}, index} ->
+      [index, inspect(instruction), holds, ba, la, pft]
     end)
     |> (& Stream.concat([~w(n d ho ba d_la_initial #{method})], &1)).()
   end
@@ -77,48 +77,63 @@ defmodule Machine.Simulator do
   def sequence_pft(result, r_fun) do
     {seq_list, {_, _, _, %{pft: pft, mdd: mdd, max_nav: max_nav}}} =
       Enum.map_reduce(result, init_seq_pft_state(), fn datum, state ->
-        datum |> secure_datum |> r_fun.() |> seq_pft_map_reducer(datum, state)
+        seq_pft_map_reducer(datum, state, r_fun)
       end)
     {{floor(pft), mdd: floor(mdd), max_pft: floor(max_nav - 1)}, seq_list}
   end
 
   defp init_seq_pft_state do
-    {0, 1.0, 1.0, %{nav: 1.0, pft: 0, max_nav: 1.0, mdd: 0}}
+    {0, 1.0, 1.0, %{nav: 1.0, pft: 0, max_nav: 1.0, dd: 0, mdd: 0,
+                    r_fun_acc: %{}}}
   end
 
-  defp next_seq_pft_state({next_holds, next_ba}, datum,
+  defp next_seq_pft_state({next_holds, next_ba}, next_r_fun_acc, datum,
        {_, _, curr_la, %{max_nav: curr_max_nav, mdd: curr_mdd}} = _state) do
     {_, _, a} = datum[:pred]
     next_la = curr_la * (1 + a)
     next_nav = next_ba + next_holds * next_la
     next_pft = next_nav - 1.0
     next_max_nav = max(curr_max_nav, next_nav)
-    next_mdd = max(curr_mdd, (curr_max_nav - next_nav) / curr_max_nav)
+    next_dd = (curr_max_nav - next_nav) / curr_max_nav
+    next_mdd = max(curr_mdd, next_dd)
     next_derived = %{nav: next_nav, pft: next_pft, max_nav: next_max_nav,
-                     mdd: next_mdd}
+                     dd: next_dd, mdd: next_mdd, r_fun_acc: next_r_fun_acc}
     {next_holds, next_ba, next_la, next_derived}
   end
 
-  defp seq_pft_mapper(decision, {holds, ba, la, %{pft: pft}} = _state) do
-    {decision, floor(holds), floor(ba), floor(la - 1), floor(pft)}
-  end
-
-  defp seq_pft_map_reducer(decision, datum, state) do
-    {condition, value_fun} = seq_decide(decision, state)
-    if condition do
-      {seq_pft_mapper(decision, state),
-       next_seq_pft_state(value_fun.(), datum, state)}
-    else
-      wrapped_decision =
-        case decision do
-          {:remain, _} -> decision
-          _            -> {:remain, decision}
-        end
-      {holds, ba, _, _} = state
-      {seq_pft_mapper(wrapped_decision, state),
-       next_seq_pft_state({holds, ba}, datum, state)}
+  defp next_r_fun_acc(decision, condition,
+                      {_, _, _, %{r_fun_acc: curr_acc}}) do
+    proposed_next_acc = Keyword.fetch!(decision, :acc)
+    cond do
+      condition ->
+        proposed_next_acc
+      Keyword.get(decision, :use_next_acc_on_remain, false) ->
+        proposed_next_acc
+      true ->
+        curr_acc
     end
   end
+
+  defp seq_pft_mapper(instruction, {holds, ba, la, %{pft: pft}} = _state) do
+    {instruction, floor(holds), floor(ba), floor(la - 1), floor(pft)}
+  end
+
+  defp seq_pft_map_reducer(datum, {holds, ba, _, _} = state, r_fun) do
+    decision = datum |> secure_datum |> r_fun.(state) |> translate_r_fun(state)
+    instruction = Keyword.fetch!(decision, :go)
+    {condition, value_fun} = decode_instruction(instruction, state)
+    r_fun_acc = next_r_fun_acc(decision, condition, state)
+    if condition do
+      {seq_pft_mapper(instruction, state),
+       next_seq_pft_state(value_fun.(), r_fun_acc, datum, state)}
+    else
+      {instruction |> wrap_remain |> seq_pft_mapper(state),
+       next_seq_pft_state({holds, ba}, r_fun_acc, datum, state)}
+    end
+  end
+
+  defp wrap_remain({:remain, _} = instruction), do: instruction
+  defp wrap_remain(instruction), do: {:remain, instruction}
 
   defp secure_datum(datum) do
     {_, p, _} = datum[:pred]
@@ -129,10 +144,10 @@ defmodule Machine.Simulator do
     |> Keyword.drop([:pred, :chunk])
   end
 
-  defp seq_decide(decision, {holds, ba, la, _derived} = _state) do
+  defp decode_instruction(instruction, {holds, ba, la, _derived} = _state) do
     slice = 0.1
     least_b_partial = 0.001
-    case decision do
+    case instruction do
       :bi_slice ->
         {ba >= slice, fn ->
           {holds + slice / la, ba - slice}
@@ -165,9 +180,16 @@ defmodule Machine.Simulator do
     end
   end
 
+  defp translate_r_fun(instruction, {_, _, _, %{r_fun_acc: r_fun_acc}})
+  when is_atom(instruction) or is_tuple(instruction) do
+    [go: instruction, acc: r_fun_acc]
+  end
+
+  defp translate_r_fun(keywords, _), do: keywords
+
   ## Sequence functions
 
-  defp seq_r_fun(:gt_threshold, datum) do
+  defp seq_r_fun(:gt_threshold, datum, _) do
     p = datum[:p]
     cond do
       is_nil(p) ->
@@ -181,7 +203,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:gt_0, datum) do
+  defp seq_r_fun(:gt_0, datum, _) do
     p = datum[:p]
     cond do
       is_nil(p) ->
@@ -193,7 +215,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:exp_1, datum) do
+  defp seq_r_fun(:exp_1, datum, _) do
     p = datum[:p]
     cond do
       is_nil(p) ->
@@ -205,7 +227,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:exp_2, datum) do
+  defp seq_r_fun(:exp_2, datum, _) do
     p = datum[:p]
     cond do
       is_nil(p) ->
@@ -217,7 +239,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:exp_3, datum) do
+  defp seq_r_fun(:exp_3, datum, _) do
     p = datum[:p]
     cond do
       is_nil(p) ->
@@ -229,7 +251,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:e4, datum) do
+  defp seq_r_fun(:e4, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -242,7 +264,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:la_gt_ma_s, datum) do
+  defp seq_r_fun(:la_gt_ma_s, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -255,7 +277,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_pure_0, datum) do
+  defp seq_r_fun(:ma_pure_0, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -268,7 +290,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_e4, datum) do
+  defp seq_r_fun(:ma_e4, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -283,7 +305,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_e5, datum) do
+  defp seq_r_fun(:ma_e5, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -298,7 +320,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_e45, datum) do
+  defp seq_r_fun(:ma_e45, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -315,7 +337,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_e45s, datum) do
+  defp seq_r_fun(:ma_e45s, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -332,7 +354,7 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_gt_0, datum) do
+  defp seq_r_fun(:ma_gt_0, datum, _) do
     p = datum[:p]
     [c1|[c2|_]] = datum[:prev_chunk]
     cond do
@@ -347,22 +369,45 @@ defmodule Machine.Simulator do
     end
   end
 
-  defp seq_r_fun(:ma_t0, datum) do
+  defp seq_r_fun(:s_t0, datum,
+                 {_holds, _ba, _la, %{dd: dd, r_fun_acc: acc}}) do
     p = datum[:p]
     [c1|[c2|[c3|_]]] = datum[:prev_chunk]
+    prev_risk_count = Map.get(acc, :risk_count, 0)
+    has_ma = has_ma?([c1, c2, c3])
+    {la_pulse, ma_pulse} =
+      if has_ma do
+        {(ma_s(c1) - la(c1)) / ma_s(c1) + 1.5 * dd,
+         (ma_l(c1) - ma_s(c1)) / ma_l(c1) + 1.5 * dd}
+      else
+        {-1, -1}
+      end
+    risk_count =
+      cond do
+        ma_pulse < 0 or la_pulse < 0 ->
+          1
+        prev_risk_count <= 0 and dd <= 0.08 ->
+          0
+        prev_risk_count <= 0 ->
+          1
+        dd <= 0.01 ->
+          0
+        true ->
+          1
+      end
+    go = fn instruction ->
+      [go: instruction, acc: put_in(acc[:risk_count], risk_count)]
+    end
+
     cond do
-      is_nil(p) or not has_ma?([c1, c2, c3]) ->
-        :of_all
-      base_e4(p, c1, c2) ->
-        :bi_all
-      base_la_gt_ma_s(p, c1, c2) ->
-        :bi_all
-      p > 0 and ma_s(c1) >= ma_s(c2) and ma_s(c2) >= ma_s(c3) ->
-        :bi_all
+      is_nil(p) or not has_ma ->
+        go.(:of_all)
       base_ma_pure_0(c1, c2) ->
-        :bi_all
+        go.(:bi_all)
+      p > 0 and risk_count <= 0 ->
+        go.(:bi_all)
       true ->
-        :of_all
+        go.(:of_all)
     end
   end
 
